28e976a9cb5e01077f0c25aeb8a3e8e868b80f4e
unknown <jaredtiu1706@gmail.com>
2024-11-08 15:45:27 +0800

    finished

---
+++ finding_bigger_number.py
@@ -0,0 +1,9 @@
+first_input = float(input("Input first number: "))
+second_input = float(input("Input second number: "))
+if first_input > second_input:
+    print(first_input)
+elif second_input > first_input:
+    print(second_input)
+elif first_input == second_input: 
+    print(first_input)
+
