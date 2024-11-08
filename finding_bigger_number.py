ad6320f7ec01e058f83591b1e64a8e03859284d8
unknown <jaredtiu1706@gmail.com>
2024-11-08 15:54:51 +0800

    added not equal

--- finding_bigger_number.py
+++ finding_bigger_number.py
@@ -1,13 +1,13 @@
 first_input = float(input("Input first number: "))
 second_input = float(input("Input second number: "))
 
-if first_input > second_input:
-    print("The biggest number is:", first_input)
-    print("The lowest number is:", second_input)
-elif second_input > first_input:
-    print("The biggest number is:", second_input)
-    print("The lowest number is:", first_input)
+if first_input != second_input:
+    print("Numbers are not equal, printing the biggest and lowest.")
+    if first_input > second_input:
+        print("The biggest number is:", first_input)
+        print("The lowest number is:", second_input)
+    elif second_input > first_input:
+        print("The biggest number is:", second_input)
+        print("The lowest number is:", first_input)
 elif first_input == second_input: 
     print("Numbers are equal")
-
-
