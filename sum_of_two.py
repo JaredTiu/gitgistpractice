9d33d4937c25589e699c7649ee2bb81a611f7237
unknown <jaredtiu1706@gmail.com>
2024-11-08 16:22:47 +0800

    added choice of sum or diff

--- sum_of_two.py
+++ sum_of_two.py
@@ -6,7 +6,11 @@ def remove_point_zero(number):
 
 first_input = float(input("Input first number: "))
 second_input = float(input("Input second number: "))
-
 sum_of_inputs = remove_point_zero(first_input + second_input)
+diff_of_inputs = remove_point_zero(first_input - second_input)
 
-print(sum_of_inputs)
+sum_or_diff = int(input("Do you want the difference or sum of the numbers?(type 1 for sum 2 for diff): "))
+if sum_or_diff == 1: 
+    print(sum_of_inputs)
+elif sum_or_diff ==2: 
+    print(diff_of_inputs)
