3f661bd2dd8194ea2c691b34e6512a7cfcdf3ea7
unknown <jaredtiu1706@gmail.com>
2024-11-08 16:28:05 +0800

    product or quotient

---
+++ product_quotient.py
@@ -0,0 +1,16 @@
+def remove_point_zero(number):
+    if number % 1 == 0:
+        return int(number)
+    else:
+        return number
+
+first_input = int(input("Input first number: "))
+second_input = int(input("Input second number: "))
+product_of_inputs = remove_point_zero(first_input * second_input)
+quotient_of_inputs = remove_point_zero(first_input / second_input)
+
+product_or_quotient = int(input("Do you want the difference or sum of the numbers?(type 1 for product 2 for quotient): "))
+if product_or_quotient == 1: 
+    print(product_of_inputs)
+elif product_or_quotient ==2: 
+    print(quotient_of_inputs)
